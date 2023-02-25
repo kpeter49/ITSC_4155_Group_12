940104afaa9bb25a0db9c641940bab06256af42d
FredFields44 <66147280+FredFields44@users.noreply.github.com>
2023-02-25 02:24:36 -0500

    Update urls.py

--- recipeapp/recipeapp/urls.py
+++ recipeapp/recipeapp/urls.py
@@ -17,5 +17,6 @@ from django.contrib import admin
 from django.urls import path
 
 urlpatterns = [
-    path("admin/", admin.site.urls),
+    path("search/", SearchResultsView.as_view(), name="search_results"),
+    path("", HomePageView.as_view(), name="home"),
 ]
