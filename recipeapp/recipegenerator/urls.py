9b47951b8fa2d819bb47c68083bb3f42350bed4d
Karl Peterson <kpeter49@uncc.edu>
2023-04-03 21:24:43 -0400

    implement navbar and setup bootstrap

--- recipeapp/recipegenerator/urls.py
+++ recipeapp/recipegenerator/urls.py
@@ -1,10 +1,11 @@
 from django.urls import include, path
 
-from .views import HomePageView, SearchResultsView, ProfileView, SignUpView
+from .views import HomePageView, SearchResultsView, ProfileView, SignUpView, RestrictionsView
 
 urlpatterns = [
     path("search/", SearchResultsView.as_view(), name="search_results"),
     path("", HomePageView.as_view(), name="home"),
+    path("restrictions", RestrictionsView.as_view(), name="restrictions"),
     path('accounts/', include('django.contrib.auth.urls')),
     path('accounts/profile/', ProfileView.as_view(), name="profile"),
     path("signup/", SignUpView.as_view(), name="signup")
--- recipeapp/recipegenerator/views.py
+++ recipeapp/recipegenerator/views.py
@@ -32,3 +32,6 @@ class SignUpView(generic.CreateView):
     form_class = UserCreationForm
     success_url = reverse_lazy("login")
     template_name = "registration/signup.html"
+
+class RestrictionsView(TemplateView):
+    template_name = 'restrictions.html'
