b9c99ba58f44e5dfce0c16b845a11bad2389a397
Karl Peterson <kpeter49@uncc.edu>
2023-04-25 09:29:52 -0400

    Updated models to include new recipes table

--- recipeapp/recipegenerator/models.py
+++ recipeapp/recipegenerator/models.py
@@ -88,3 +88,24 @@ class RestrictionPresetIngredient(models.Model):
 
     def __str__(self):
         return self.name
+    
+class Recipes(models.Model):
+    id = models.IntegerField(blank=True, null=True)
+    recipename = models.TextField(db_column='RecipeName', blank=True, null=True)  # Field name made lowercase.
+    preptime = models.TextField(db_column='PrepTime', blank=True, null=True)  # Field name made lowercase.
+    cooktime = models.TextField(db_column='CookTime', blank=True, null=True)  # Field name made lowercase.
+    totaltime = models.TextField(db_column='TotalTime', blank=True, null=True)  # Field name made lowercase.
+    servings = models.IntegerField(db_column='Servings', blank=True, null=True)  # Field name made lowercase.
+    yeild = models.TextField(db_column='Yeild', blank=True, null=True)  # Field name made lowercase.
+    ingredients = models.TextField(db_column='Ingredients', blank=True, null=True)  # Field name made lowercase.
+    directions = models.TextField(db_column='Directions', blank=True, null=True)  # Field name made lowercase.
+    rating = models.FloatField(db_column='Rating', blank=True, null=True)  # Field name made lowercase.
+    link = models.TextField(db_column='Link', blank=True, null=True)  # Field name made lowercase.
+    cuisinetype = models.TextField(db_column='CuisineType', blank=True, null=True)  # Field name made lowercase.
+    nutrition = models.TextField(db_column='Nutrition', blank=True, null=True)  # Field name made lowercase.
+    timing = models.TextField(db_column='Timing', blank=True, null=True)  # Field name made lowercase.
+    img = models.TextField(db_column='Img', blank=True, null=True)  # Field name made lowercase.
+
+    class Meta:
+        managed = False
+        db_table = 'recipes'
